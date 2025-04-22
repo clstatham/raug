3af5958cf098f1ee6d57961b239059d44e847f23
clstatham <58794204+clstatham@users.noreply.github.com>
2025-04-21 20:26:50 -0500

    added caterpillar example that i've been using internally for months now

---
+++ examples/caterpillar.rs
@@ -0,0 +1,129 @@
+use std::f32::consts::PI;
+
+use raug::prelude::*;
+use raug_ext::prelude::*;
+
+pub fn pick_randomly(graph: &Graph, trig: &Node, options: &[f32]) -> Node {
+    let node = graph.add(RandomChoice::<f32>::default());
+    node.input("trig").connect(trig.output(0));
+    node.input("options")
+        .connect(graph.constant(List::from_slice(options)));
+    node
+}
+
+pub fn fm_sine_osc(graph: &Graph, freq: &Node, mod_freq: &Node) -> Node {
+    let sr = graph.add(SampleRate::default());
+    let phase = graph.add(PhaseAccumulator::default());
+    let increment = freq / sr;
+    phase.input(0).connect(increment.output(0));
+    (phase * 2.0f32 * PI + mod_freq * 2.0f32 * PI).sin()
+}
+
+pub fn midi_to_freq(midi: f32) -> f32 {
+    440.0 * f32::powf(2.0, (midi - 69.0) / 12.0)
+}
+
+pub fn scale_freqs(detune: f32) -> Vec<f32> {
+    // minor scale
+    let scale = [0, 2, 3, 5, 7, 8, 10];
+    let base = 48; // C3
+    let mut freqs = vec![];
+    for note in &scale {
+        freqs.push(midi_to_freq(base as f32 + *note as f32 + detune));
+    }
+    let base = 60;
+    for note in &scale {
+        freqs.push(midi_to_freq(base as f32 + *note as f32 + detune));
+    }
+    let base = 36;
+    for note in &scale {
+        freqs.push(midi_to_freq(base as f32 + *note as f32 + detune));
+    }
+    freqs
+}
+
+pub fn random_tones(
+    graph: &Graph,
+    rates: &[f32],
+    ratios: &[f32],
+    freqs: &[f32],
+    decays: &[f32],
+    amps: &[f32],
+) -> Node {
+    let mast = graph.add(Metro::default());
+    mast.input(0).connect(rates[0]);
+
+    let rate = pick_randomly(graph, &mast, rates).unwrap_or(0.0f32);
+
+    let trig = graph.add(Metro::default());
+    trig.input(0).connect(rate.output(0));
+
+    let freq = pick_randomly(graph, &trig, freqs).unwrap_or(440.0f32);
+
+    let amp_decay = pick_randomly(graph, &trig, decays).unwrap_or(0.0f32);
+
+    let ratio = pick_randomly(graph, &trig, ratios).unwrap_or(0.0f32);
+
+    let amp = pick_randomly(graph, &trig, amps).unwrap_or(0.0f32);
+
+    // create the amplitude envelope
+
+    let amp_env = graph.add(DecayEnv::new(1.0f32));
+    amp_env.input("tau").connect(amp_decay.output(0));
+    amp_env.input("trig").connect(trig.output(0));
+
+    // create the modulator
+    let modulator = graph.add(BlSawOscillator::default());
+    modulator.input(0).connect((&freq * ratio).output(0));
+
+    // create the carrier
+    let carrier = fm_sine_osc(graph, &freq, &(modulator * 0.1f32));
+
+    carrier * amp_env * amp
+}
+
+pub fn caterpillar(num_tones: usize) -> Graph {
+    let ratios = &[0.25, 0.5, 1.0, 2.0];
+    let decays = &[0.02, 0.1, 0.2, 0.5];
+    let amps = &[0.125, 0.25, 0.5, 0.8];
+    let rates = &[1. / 8., 1. / 4., 1. / 2., 1., 2.];
+
+    let freqs = scale_freqs(0.0);
+
+    let graph = Graph::new();
+
+    let out1 = graph.add_audio_output();
+    let out2 = graph.add_audio_output();
+
+    let mut tones = vec![];
+    for _ in 0..num_tones {
+        let tone = random_tones(&graph, rates, ratios, &freqs, decays, amps);
+        tones.push(tone);
+    }
+
+    let mut mix = tones[0].clone();
+    for tone in tones.iter().skip(1) {
+        mix = mix.clone() + tone.clone();
+    }
+
+    let mix = mix * 0.1f32;
+
+    let master = graph.add(PeakLimiter::default());
+    master.input(0).connect(mix.output(0));
+
+    master.output(0).connect(&out1.input(0));
+    master.output(0).connect(&out2.input(0));
+
+    graph
+}
+
+fn main() {
+    let graph = caterpillar(8);
+
+    let mut stream = CpalStream::default();
+    stream.spawn(&graph).unwrap();
+    stream.play().unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    stream.stop().unwrap();
+    stream.join().unwrap();
+}
